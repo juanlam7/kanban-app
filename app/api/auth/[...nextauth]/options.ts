97ae2563362dd4ddc5cf9f73c12b57db65227198
juanlam7 <juanlam7@gmail.com>
2024-11-02 16:09:55 +0100

    feat: first step, creating login feature and reusables components

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,78 @@
+import { GraphQLClient } from "graphql-request";
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+interface LoginResponse {
+  login: {
+    value: string;
+    id: string;
+    name: string;
+    username: string;
+  };
+}
+
+const graphqlClient = new GraphQLClient(process.env.GRAPHQL_ENDPOINT!);
+
+const mutation = `
+    mutation Login($username: String!, $password: String!) {
+      login(username: $username, password: $password) {
+        value
+      }
+    }
+`;
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        try {
+          const variables = {
+            username: credentials?.username,
+            password: credentials?.password,
+          };
+
+          const data: LoginResponse = await graphqlClient.request(
+            mutation,
+            variables
+          );
+
+          if (data) {
+            return {
+              id: data.login.id ?? "testId", // make login return id
+              name: data.login.name ?? "name", // make login return name
+              username: data.login.username ?? "username", // make login return username
+              token: data.login.value,
+            };
+          }
+
+          return null;
+        } catch (error) {
+          console.error("Error during login:", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      session.user = token as any;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="h-screen flex items-center justify-center">
+        <p>Welcome</p>
+      </section>
+    </main>
+  );
+}
